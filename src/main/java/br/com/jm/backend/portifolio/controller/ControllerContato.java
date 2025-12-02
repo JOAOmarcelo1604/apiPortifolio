9e9f9417dac4e1b8517b948de628f9d9b5fe3b22
unknown <marcelo.oliveira@DESKTOP-LP8I4M3.gsr.local>
2025-12-02 19:56:23 -0300

    git da deixar end-point ativado, cronojob

--- src/main/java/br/com/jm/backend/portifolio/controller/ControllerContato.java
+++ src/main/java/br/com/jm/backend/portifolio/controller/ControllerContato.java
@@ -5,10 +5,7 @@ import br.com.jm.backend.portifolio.model.ContatoModel;
 import br.com.jm.backend.portifolio.services.IServiceContato;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ControllerContato {
@@ -24,5 +21,11 @@ public class ControllerContato {
             return ResponseEntity.status(201).body(res);
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/ativado")
+    @CrossOrigin(origins = "*")
+    public String isAtivado() {
+        return "Ativado";
     }
 }
